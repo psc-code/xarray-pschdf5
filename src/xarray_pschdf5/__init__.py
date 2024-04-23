1e3fe22bf17718bcb69253e0d04e78bb503a42cf
Kai Germaschewski <kai.germaschewski@unh.edu>
2024-04-22 22:30:37 -0400

    initial commit made from "cookie"
    https://github.com/scientific-python/cookie

---
+++ src/xarray_pschdf5/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2024 Kai Germaschewski. All rights reserved.
+
+xarray-pschdf5: XArray reader for PSC HDF5 data
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
