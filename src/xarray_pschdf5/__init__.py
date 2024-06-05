50a1c2c92828778783175783f634c0373d984d5e
Kai Germaschewski <kai@kais-mbp.aw4.unh.edu>
2024-06-05 11:54:43 -0400

    changes from "pre-commit"

--- src/xarray_pschdf5/__init__.py
+++ src/xarray_pschdf5/__init__.py
@@ -9,5 +9,3 @@ from __future__ import annotations
 from ._version import version as __version__
 
 __all__ = ["__version__"]
-
-from .pschdf5_backend import PscHdf5Entrypoint
--- src/xarray_pschdf5/pschdf5_backend.py
+++ src/xarray_pschdf5/pschdf5_backend.py
@@ -1,10 +1,13 @@
-from collections import OrderedDict
-from pugixml import pugi
-import h5py
-import xarray as xr
-from xarray.backends import BackendEntrypoint
-import numpy as np
+from __future__ import annotations
+
 import os
+from collections import OrderedDict
+
+import h5py
+import numpy as np
+import xarray as xr
+from pugixml import pugi
+from xarray.backends import BackendEntrypoint
 
 
 class PscHdf5Entrypoint(BackendEntrypoint):
@@ -43,86 +46,97 @@ def pschdf5_open_dataset(filename_or_obj, *, drop_variables=None):
         for fldname, fld in grid["fields"].items():
             data_dims = fld["dims"]
             data_path = fld["path"]
-            h5_filename, h5_path = fld['path'].split(':')
-            h5_file = h5py.File(dirname + '/' + h5_filename)
+            h5_filename, h5_path = fld["path"].split(":")
+            h5_file = h5py.File(dirname + "/" + h5_filename)
             data = h5_file[h5_path][:].T
 
             data_attrs = dict(path=data_path)
             vars[fldname] = xr.DataArray(
-                data=data,
-                dims=["x", "y", "z"],
-                attrs=data_attrs)
+                data=data, dims=["x", "y", "z"], attrs=data_attrs
+            )
 
-    coords = {"xyz"[d]: make_crd(grid['topology']['dims'][d], grid['geometry']
-                                 ['origin'][d], grid['geometry']['spacing'][d]) for d in range(3)}
+    coords = {
+        "xyz"[d]: make_crd(
+            grid["topology"]["dims"][d],
+            grid["geometry"]["origin"][d],
+            grid["geometry"]["spacing"][d],
+        )
+        for d in range(3)
+    }
 
     attrs = dict(run=meta["run"], time=meta["time"])
 
     ds = xr.Dataset(vars, coords=coords, attrs=attrs)
-#    ds.set_close(my_close_method)
+    #    ds.set_close(my_close_method)
 
     return ds
 
 
 def make_crd(dim, origin, spacing):
-    return origin + np.arange(.5, dim) * spacing
+    return origin + np.arange(0.5, dim) * spacing
 
 
 def read_xdmf(filename):
     doc = pugi.XMLDocument()
     result = doc.load_file(filename)
     if not result:
-        print('parse error: status=%r description=%r' %
-              (result.status, result.description()))
+        print(
+            "parse error: status=%r description=%r"
+            % (result.status, result.description())
+        )
         assert False
 
-    grid_collection = doc.child('Xdmf').child('Domain').child('Grid')
-    assert grid_collection.attribute('GridType').value() == 'Collection'
-    grid_time = grid_collection.child('Time')
-    assert grid_time.attribute('Type').value() == 'Single'
-    time = grid_time.attribute('Value').value()
+    grid_collection = doc.child("Xdmf").child("Domain").child("Grid")
+    assert grid_collection.attribute("GridType").value() == "Collection"
+    grid_time = grid_collection.child("Time")
+    assert grid_time.attribute("Type").value() == "Single"
+    time = grid_time.attribute("Value").value()
     rv = {}
-    rv['time'] = time
-    rv['grids'] = {}
+    rv["time"] = time
+    rv["grids"] = {}
     for node in grid_collection.children():
-        if node.name() == 'Grid':
+        if node.name() == "Grid":
             grid = {}
-            grid_name = node.attribute('Name').value()
-            topology = node.child('Topology')
+            grid_name = node.attribute("Name").value()
+            topology = node.child("Topology")
             # assert topology.attribute('TopologyType').value() == '3DCoRectMesh'
-            dims = topology.attribute('Dimensions').value()
+            dims = topology.attribute("Dimensions").value()
             dims = np.asarray([int(d) - 1 for d in dims.split(" ")])[::-1]
-            grid['topology'] = {'type': topology.attribute(
-                'TopologyType').value(), 'dims': dims}
+            grid["topology"] = {
+                "type": topology.attribute("TopologyType").value(),
+                "dims": dims,
+            }
 
-            geometry = node.child('Geometry')
-            assert geometry.attribute(
-                'GeometryType').value() == 'Origin_DxDyDz'
+            geometry = node.child("Geometry")
+            assert geometry.attribute("GeometryType").value() == "Origin_DxDyDz"
             for child in geometry.children():
-                if child.attribute('Name').value() == 'Origin':
+                if child.attribute("Name").value() == "Origin":
                     origin = np.asarray(
-                        [float(x) for x in child.text().as_string().split(" ")])[::-1]
-                if child.attribute('Name').value() == 'Spacing':
+                        [float(x) for x in child.text().as_string().split(" ")]
+                    )[::-1]
+                if child.attribute("Name").value() == "Spacing":
                     spacing = np.asarray(
-                        [float(x) for x in child.text().as_string().split(" ")])[::-1]
+                        [float(x) for x in child.text().as_string().split(" ")]
+                    )[::-1]
 
-            grid['geometry'] = {'origin': origin, 'spacing': spacing}
+            grid["geometry"] = {"origin": origin, "spacing": spacing}
 
             flds = OrderedDict()
             for child in node.children():
-                if child.name() != 'Attribute':
+                if child.name() != "Attribute":
                     continue
 
-                fld = child.attribute('Name').value()
-                item = child.child('DataItem')
-                fld_dims = np.asarray([int(d) for d in item.attribute(
-                    'Dimensions').value().split(" ")])[::-1]
+                fld = child.attribute("Name").value()
+                item = child.child("DataItem")
+                fld_dims = np.asarray(
+                    [int(d) for d in item.attribute("Dimensions").value().split(" ")]
+                )[::-1]
                 assert np.all(fld_dims == dims)
-                assert item.attribute('Format').value() == 'HDF'
+                assert item.attribute("Format").value() == "HDF"
                 path = item.text().as_string()
-                flds[fld] = {'path': path, 'dims': dims}
+                flds[fld] = {"path": path, "dims": dims}
 
-            grid['fields'] = flds
-            rv['grids'][grid_name] = grid
+            grid["fields"] = flds
+            rv["grids"][grid_name] = grid
 
     return rv
